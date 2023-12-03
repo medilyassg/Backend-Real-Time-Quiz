7809058916a77f932fc97fb39a83e009f0aa0c91
Youssef Dalouji <daloujiy@gmail.com>
2023-12-03 18:16:43 +0000

    Config Route Api

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,24 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 
-Route::post('login',[LoginController::class,'login']);
-Route::post('logout',[LoginController::class,'logout']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::middleware(['web', 'api'])->group(function () {
+
+    Route::prefix('v1')->group(function () {
+        Route::group(['prefix' => 'auth'], function () {
+
+            Route::post('login',[LoginController::class,'login']);
+
+            Route::post('logout',[LoginController::class,'logout'])->middleware('auth:sanctum');
+        });
+
+        Route::middleware(['auth:sanctum'])->group(function () {
+            // All Route Application
+            Route::get('user',[LoginController::class,'user']);
+        });
+
+    });
 });
+
+
