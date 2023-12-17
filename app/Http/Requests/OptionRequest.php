b3ef1ec4b8327dea57106904fa37c59bc20193d1
medilyassg <elghazi.mohamed.ilyass@gmail.com>
2023-12-17 16:36:52 +0100

    Add Option Resource and related functionality

---
+++ app/Http/Requests/OptionRequest.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class OptionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true; // You can customize this based on your authorization logic
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'text' => 'required|string',
+            'correct' => 'nullable|boolean',
+            'questionId' => 'nullable|exists:questions,id',
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\QuizController;
 use App\Http\Controllers\RegistreController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\OptionController;
 
 
 
@@ -18,6 +19,7 @@ Route::middleware(['web', 'api'])->group(function () {
         });
 
         Route::post('registre', [RegistreController::class, 'registre']);
+
         Route::middleware(['auth:sanctum'])->group(function () {
             // All Route Application
             Route::get('user', [LoginController::class, 'user']);
@@ -28,6 +30,15 @@ Route::middleware(['web', 'api'])->group(function () {
                 Route::put('{quiz}', [QuizController::class, 'update']);
                 Route::delete('{quiz}', [QuizController::class, 'destroy']);
             });
+            
+            Route::prefix('options')->group(function () {
+                Route::get('/', [OptionController::class, 'index']);
+                Route::get('/{id}', [OptionController::class, 'show']);
+                Route::post('/', [OptionController::class, 'store']);
+                Route::put('/{id}', [OptionController::class, 'update']);
+                Route::delete('/{id}', [OptionController::class, 'destroy']);
+            });
+
         });
     });
 });
