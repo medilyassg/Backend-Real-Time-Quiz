e576be6dd8f6d02c0df9e238c182be8f8cd99b9b
ayoub <ayoubaarab571@gmail.com>
2023-12-17 13:25:01 +0100

    modification des routes

--- routes/api.php
+++ routes/api.php
@@ -2,31 +2,32 @@
 
 use App\Http\Controllers\QuizController;
 use App\Http\Controllers\RegistreController;
-use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
-Route::prefix('v1')->group(function () {
 
-    // Authentication Routes
-    Route::group(['prefix' => 'auth'], function () {
-        Route::post('login', [LoginController::class, 'login']);
-        Route::post('logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
-    });
 
-    // Registration Route
-    Route::post('registre', [RegistreController::class, 'registre']);
+Route::middleware(['web', 'api'])->group(function () {
 
-    // Authenticated Routes
-    Route::middleware(['auth:sanctum'])->group(function () {
-        // User Route
-        Route::get('user', [LoginController::class, 'user']);
+    Route::prefix('v1')->group(function () {
+        Route::group(['prefix' => 'auth'], function () {
 
-        // Quiz Routes
-        Route::prefix('quizzes')->group(function () {
-            Route::get('/', [QuizController::class, 'index']);
-            Route::post('/', [QuizController::class, 'store']);
-            Route::put('{quiz}', [QuizController::class, 'update']);
-            Route::delete('{quiz}', [QuizController::class, 'destroy']);
+            Route::post('login', [LoginController::class, 'login']);
+
+            Route::post('logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
+        });
+
+        Route::post('registre', [RegistreController::class, 'registre']);
+        Route::middleware(['auth:sanctum'])->group(function () {
+            // All Route Application
+            Route::get('user', [LoginController::class, 'user']);
+
+            Route::prefix('quizzes')->group(function () {
+                Route::get('/', [QuizController::class, 'index']);
+                Route::post('/', [QuizController::class, 'store']);
+                Route::put('{quiz}', [QuizController::class, 'update']);
+                Route::delete('{quiz}', [QuizController::class, 'destroy']);
+            });
         });
     });
 });
